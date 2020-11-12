ca492463db4120736d8b159acdc5bfd0b467ddac
freddy0506 <46132734+freddy0506@users.noreply.github.com>
2020-11-12 18:08:53 +0100

    Update Start_Script.py

--- Vertretungsplan/Start_Script.py
+++ Vertretungsplan/Start_Script.py
@@ -6,19 +6,20 @@ path = "C:/Users/Frede/OneDrive/Dokumente/Vertretungspläne/Server/Vertretungspl
 posClasses = ["5a", "5b", "5c", "5d", "5e", "6a", "6b", "6c", "6d", "6e", "7a", "7b", 
              "7c", "7d", "7e" ,"8a", "8b", "8c", "8d", "8e", "9a", "9b", "9c", "9d", "9e", "EF", "Q1", "Q2"]
 
-while True:
-    if Vertretungsplan.reloadtime(3):
-        Date, Weekday, Year = Vertretungsplan.getDate()
-        #Schoolclass, Date = Vertretungsplan.readInfofile(path)
+#while True:
+#    if Vertretungsplan.reloadtime(3):
 
-        pages = Vertretungsplan.Download()
+Date, Weekday, Year = Vertretungsplan.getDate()
+#Schoolclass, Date = Vertretungsplan.readInfofile(path)
 
-        Vertretungsplan.HtmltoCsv(pages, Year)
+pages = Vertretungsplan.Download()
 
-        #Vertretungsplan.searchVertretungen(Schoolclass, Date)
-        Vertretungsplan.sortAllClass(posClasses, Date, path)
+Vertretungsplan.HtmltoCsv(pages, Year)
 
-        #Vertretungsplan.writeInHtml(path, Date, Weekday)
-        Vertretungsplan.writeAll(Date, Weekday, posClasses, path)
-        print("Updated")
-        time.sleep(60)
+#Vertretungsplan.searchVertretungen(Schoolclass, Date)
+Vertretungsplan.sortAllClass(posClasses, Date, path)
+
+#Vertretungsplan.writeInHtml(path, Date, Weekday)
+Vertretungsplan.writeAll(Date, Weekday, posClasses, path)
+print("Updated")
+time.sleep(60)
